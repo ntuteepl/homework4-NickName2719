e0a0bd4481b09c2eb59062592e23dd14e686b445
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-30 22:51:01 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#define N 50
+
+int main ( ) {
+    int n ;
+    scanf ( "%d" , &n ) ;
+    assert ( n>=1 && n<=50 ) ;
+    int i , j ; // I¡G°}¦C²Ä´X­Ó¡AJ¡G
+    int a[N] ;
+    for ( i=0 ; n>i ; i++ ) {
+        scanf ( "%d", &a[i] ) ;
+        for ( j=2 ; a[i]>j ; j++ ) {
+            if ( a[i]%j==0 ) {
+                printf ( "NO " ) ;
+                break ;
+            }
+        }
+        if ( a[i]==1 ) {
+            printf ( "NO " ) ;
+        }
+        else if ( a[i]<=j ) {
+            printf ( "YES " ) ;
+        }
+    }
+    return 0;
+}
 
